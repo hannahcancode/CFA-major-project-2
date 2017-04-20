2edb98cfff6b28224ee9015892a19419a6f226ef
Hannah Thompson <thompson.h@gmail.com>
2017-04-20 17:14:13 +1000

    added cards to spaces index

--- app/models/space.rb
+++ app/models/space.rb
@@ -3,6 +3,6 @@ class Space < ApplicationRecord
   has_many :bookings, :dependent => :destroy
 
   mount_uploaders :images, ImagesUploader
-  serialize :avatars, JSON # If you use SQLite, add this line.
+  serialize :images, JSON # If you use SQLite, add this line.
 
 end
