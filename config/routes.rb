a5ec499e837fc3e9ec1ce954d8a109abdce5ddb2
Hannah Thompson <thompson.h@gmail.com>
2017-04-20 14:41:25 +1000

    add guest and host dashboard to view bookings

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   end
   resources :profiles
   devise_for :users
+  get 'hostdashboard', to: 'pages#host_dashboard'
+  get 'guestdashboard', to: 'pages#guest_dashboard'
   root 'pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
