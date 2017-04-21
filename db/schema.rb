589dd5995176a4327b60f9bde820fd46a7ba43ea
Hannah Thompson <thompson.h@gmail.com>
2017-04-21 13:07:10 +1000

    create conversations and messages migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420081151) do
+ActiveRecord::Schema.define(version: 20170421030310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,20 @@ ActiveRecord::Schema.define(version: 20170420081151) do
     t.boolean  "paid",       default: false
     t.index ["space_id"], name: "index_bookings_on_space_id", using: :btree
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
+  end
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "recipient_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text    "body"
+    t.integer "conversation_id"
+    t.integer "user_id"
+    t.boolean "read",            default: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
+    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
 
   create_table "products", force: :cascade do |t|
